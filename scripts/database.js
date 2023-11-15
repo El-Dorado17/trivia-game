6c3db5750c39dba68cfbf3dd8d0e1954e16ecb8c
Ely Dorado <2018dorado@gmail.com>
2023-11-15 13:24:58 -0600

    working on browser

--- scripts/database.js
+++ scripts/database.js
@@ -1,42 +1,43 @@
-database=[
-    {
+const database = {
+    questions: [{
         id: 1,
-        question: "What year is known as Y2K?",
-        answer: "2000",
+        q: "What year is known as Y2K?",
+        a: "2000",
         wrong1: "1999",
         wrong2: "2001",
         wrong3: "1492"
-    },
-    {
+    }, {
         id: 2,
-        question: "How would you pronounce the musical key: 'A#' ?",
-        answer: "A-Sharp",
+        q: "How would you pronounce the musical key: 'A#' ?",
+        a: "A-Sharp",
         wrong1: "A-Pound",
         wrong2: "A-Hash",
         wrong3: "A-Number"
-    },
-    {
+    }, {
         id: 3,
-        question: "Who were the founding twins of Rome, raised by Faustulus?",
-        answer: "Romulus & Remus",
+        q: "Who were the founding twins of Rome, raised by Faustulus?",
+        a: "Romulus & Remus",
         wrong1: "Mario & Luigi",
         wrong2: "Prof. Lupin & Sirius Black",
         wrong3: "Ben & Jerry"
-    },
-    {
+    }, {
         id: 4,
-        question: "What is the chemical symbol for gold?",
-        answer: "Au",
+        q: "What is the chemical symbol for gold?",
+        a: "Au",
         wrong1: "Go",
         wrong2: "Or",
         wrong3: "Ag"
-    },
-    {
+    }, {
         id: 5,
-        question: "Which of these events occured in 1966?",
-        answer: "All other choices",
+        q: "Which of these events occured in 1966?",
+        a: "All other choices",
         wrong1: "The Beatles released their album 'Revolver'",
         wrong2: "England hosted and won the World Cup against West Germany at Wembley Stadium",
         wrong3: "The first episode of the science fiction television series 'Star Trek' aired"
-    },
-]
+    }
+    ]
+}
+
+export const getQuestions = () => {
+    return database.questions.map(question => ({...question}))
+}
